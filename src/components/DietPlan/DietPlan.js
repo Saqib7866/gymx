f77401fc6008d2665af123e1ccb455be3c501ded
Bilal Khalid <bilalkhalid1000@gmail.com>
2020-08-07 03:51:55 +0500

    Added backgound picture in diet plan.

--- src/components/DietPlan/DietPlan.js
+++ src/components/DietPlan/DietPlan.js
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 
 import { Card, CardHeader, CardTitle, CardBody } from "reactstrap";
 
+import background from "../../assets/img/theme/workout.jpg";
+
 import DietTable from "components/BMR/DietTable";
 import AppContext from "Context/AppContext";
 
@@ -9,34 +11,43 @@ export default class DietPlan extends Component {
   static contextType = AppContext;
   render() {
     return (
-      <div className="container mt-9 mb-9">
-        <Card className="p-3 shadow">
-          <CardTitle className="h2 container">Diet Plan</CardTitle>
-          <CardBody>
-            <DietTable
-              rice={
-                this.context.user.diet_plan
-                  ? this.context.user.diet_plan.rice
-                  : this.context.dietTable.rice
-              }
-              chicken={
-                this.context.user.diet_plan
-                  ? this.context.user.diet_plan.chicken
-                  : this.context.dietTable.chicken
-              }
-              olive={
-                this.context.user.diet_plan
-                  ? this.context.user.diet_plan.olive
-                  : this.context.dietTable.olive
-              }
-              egg={
-                this.context.user.diet_plan
-                  ? this.context.user.diet_plan.egg
-                  : this.context.dietTable.egg
-              }
-            />
-          </CardBody>
-        </Card>
+      <div
+        className="vh-100"
+        style={{
+          backgroundImage: `url(${background})`,
+          backgroundSize: "cover",
+          backgroundRepeat: "no-repeat",
+        }}
+      >
+        <div className="container pt-9 mb-9">
+          <Card className="p-3 shadow">
+            <CardTitle className="h2 container">Diet Plan</CardTitle>
+            <CardBody>
+              <DietTable
+                rice={
+                  this.context.user.diet_plan
+                    ? this.context.user.diet_plan.rice
+                    : this.context.dietTable.rice
+                }
+                chicken={
+                  this.context.user.diet_plan
+                    ? this.context.user.diet_plan.chicken
+                    : this.context.dietTable.chicken
+                }
+                olive={
+                  this.context.user.diet_plan
+                    ? this.context.user.diet_plan.olive
+                    : this.context.dietTable.olive
+                }
+                egg={
+                  this.context.user.diet_plan
+                    ? this.context.user.diet_plan.egg
+                    : this.context.dietTable.egg
+                }
+              />
+            </CardBody>
+          </Card>
+        </div>
       </div>
     );
   }
