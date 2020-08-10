ec4d1e936b6e29b87840d7f2bd274f52756c56d3
Bilal <bilalkhalid1000@gmail.com>
2020-08-10 19:46:58 +0500

    Updated ndashboard.js to index.js

--- src/index.js
+++ src/index.js
@@ -22,7 +22,7 @@ const DietPlan = lazy(() => import("./components/DietPlan/DietPlan"));
 const Nuts = lazy(() => import("./views/examples/nutritionists"));
 const Main = lazy(() => import("./components/BMR/Main"));
 const Userdash = lazy(() => import("./views/examples/userdash"));
-const NDashboard = lazy(() => import("./components/NDashboard/NDashboard"));
+const NDashboard = lazy(() => import("./components/NDashboard"));
 const FinalWorkout = lazy(() => import("./components/Workout/FinalWorkout"));
 const Events = lazy(() => import("./views/examples/EventCardDisplay"));
 const PlaceOrder = lazy(() => import("./views/examples/PlaceOrder"));
